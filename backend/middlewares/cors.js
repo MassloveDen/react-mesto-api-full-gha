7462d561f5bdb675695754df054ea0ebdc42c021
MassloveDen <denis132141@yandex.ru>
2023-08-07 17:23:31 +0000

    fix routes

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,33 +1,33 @@
 const allowedCors = [
-    // 'https://project-mesto.nomoredomains.monster',
-    // 'http://project-mesto.nomoredomains.monster',
-    // 'https://api.project-mesto.nomoredomains.monster/users/me',
-    // 'https://api.project-mesto.nomoredomains.monster/cards',
-    // 'https://api.project-mesto.nomoredomains.monster/signup',
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://localhost:3001',
-    'http://localhost:4000',
-    // 'https://130.193.48.152',
-    // 'http://130.193.48.152',
-  ];
-  
-  module.exports = (req, res, next) => {
-    const { origin } = req.headers;
-    const { method } = req;
-    const requestHeaders = req.headers['access-control-request-headers'];
-    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  
-    if (allowedCors.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-      res.header('Access-Control-Allow-Credentials', true);
-    }
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-  
-      return res.end();
-    }
-  
-    return next();
-  };
+  'https://masslove.back.nomoreparties.co',
+  'http://masslove.back.nomoreparties.co',
+  'https://masslove.back.nomoreparties.co/users/me',
+  'https://masslove.back.nomoreparties.co/cards',
+  'https://masslove.back.nomoreparties.co/signup',
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3001',
+  'http://localhost:4000',
+  'https://158.160.46.173',
+  'http://158.160.46.173',
+];
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.end();
+  }
+
+  return next();
+};
